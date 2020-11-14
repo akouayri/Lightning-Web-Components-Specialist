a1bde0f6aa06131f042fe52dcb5c7b4d6fb8f7d8
Abhishek Saxena <abhisheksaxena2.12@gmail.com>
2020-11-13 23:43:31 -0500

    LWC and SF Data: Handle errors - 2

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from "lwc";
+import { reduceErrors } from "c/ldsUtils";
 import FIRST_NAME_FIELD from "@salesforce/schema/Contact.FirstName";
 import LASTNAME_FIELD from "@salesforce/schema/Contact.LastName";
 import EMAIL_FIELD from "@salesforce/schema/Contact.Email";
@@ -24,4 +25,7 @@ export default class contactList extends LightningElement {
   columns = COLUMNS;
   @wire(getContacts)
   contacts;
+  get errors() {
+    return this.contacts.error ? reduceErrors(this.contacts.error) : [];
+  }
 }
